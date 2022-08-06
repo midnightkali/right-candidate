5918c71fa7c05458d302e53430ef42e13836a171
MidnightKali <86851068+midnightkali@users.noreply.github.com>
2022-08-06 00:58:42 -0400

    Update laptop-battery-monitoring.ps1

--- system-sctipts/laptop-battery-monitoring/laptop-battery-monitoring.ps1
+++ system-sctipts/laptop-battery-monitoring/laptop-battery-monitoring.ps1
@@ -1,2 +1,15 @@
-powercfg /batteryreport /XML /OUTPUT "batterydata.xml"
-[xml]$battData = Get-Content "batterydata.xml"
+powercfg /batteryreport /XML /OUTPUT "batteryreport.xml"
+[xml]$battData = Get-Content "batteryreport.xml"
+
+$batteryLevel = $battData.BatteryReport.Batteries | ForEach-Object {
+    [PSCustomObject]@{
+    DesignCapacity = $_.Battery.DesignCapacity
+    FullChargeCapacity = $_.Battery.FullChargeCapacity
+    CycleCount = $_.Battery.CycleCount
+    Id = $_.Battery.id
+    }
+}
+
+if (!$BatteryStatus) {
+    Write-Host "Are you sure your laptop has batteries? This wont work with desktop computers"
+    }
