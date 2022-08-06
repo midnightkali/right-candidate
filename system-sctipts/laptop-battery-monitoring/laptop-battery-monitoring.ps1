9a3082c14c276a05858c9946ea07848e7b653c97
MidnightKali <86851068+midnightkali@users.noreply.github.com>
2022-08-06 17:48:09 -0400

    Update laptop-battery-monitoring.ps1

--- system-sctipts/laptop-battery-monitoring/laptop-battery-monitoring.ps1
+++ system-sctipts/laptop-battery-monitoring/laptop-battery-monitoring.ps1
@@ -1,19 +1,23 @@
 powercfg /batteryreport /XML /OUTPUT "batteryreport.xml"
+
 [xml]$battData = Get-Content "batteryreport.xml"
 
 $batteryLevel = $battData.BatteryReport.Batteries | ForEach-Object {
+    
     [PSCustomObject]@{
-    designCapacity = $_.Battery.DesignCapacity
-    fullChargeCapacity = $_.Battery.FullChargeCapacity
-    cycleCount = $_.Battery.CycleCount
-    id = $_.Battery.id
+    DesignCapacity = $_.Battery.DesignCapacity
+    FullChargeCapacity = $_.Battery.FullChargeCapacity
+    CycleCount = $_.Battery.CycleCount
+    Id = $_.Battery.id
+    Chemistry = $_.Battery.Chemistry
     }
+    
 }
 
-if (!$batteryStatus) {
+if (!$batteryLevel) {
     Write-Host "Are you sure your laptop has batteries? This wont work with desktop computers"
     }
 
-foreach ($battery in $batteryStatus) {
-
+foreach ($battery in $batteryLevel) {
+    Write-host "You are using $($battery.Chemistry) You have a capacity of $($battery.DesignCapacity) and $($battery.FullChargeCapacity) left. You're battery info is $($battery.id)"
 }
