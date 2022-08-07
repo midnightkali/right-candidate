6859dffe918e087aefd848c6a271a6d07d6f9e4d
MidnightKali <86851068+midnightkali@users.noreply.github.com>
2022-08-06 22:29:54 -0400

    Update monitor-cpu-bash.sh

--- system-sctipts/monitor-cpu-bash/monitor-cpu-bash.sh
+++ system-sctipts/monitor-cpu-bash/monitor-cpu-bash.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+while [ true ] ;do
+curUsed=`free -m |awk 'NR==3 {print $4}'`
+
+if [ $curUsed -lt 1000 ] && [ $curUsed -gt 800 ]; 
+
+then
+echo "Alert below 800" | /bin/mail -s "Memory treshold limit reached!!" patmagpantay22@icloud.com
+
+
