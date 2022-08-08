232a0c20af2e82b03ceb6e0b6d6da91f67c847e4
MidnightKali <86851068+midnightkali@users.noreply.github.com>
2022-08-08 01:51:12 -0400

    Update monitor-services.ps1

--- system-sctipts/monitor-services/monitor-services.ps1
+++ system-sctipts/monitor-services/monitor-services.ps1
@@ -1,4 +1,12 @@
 $dirFolder = "C:\promedica\"
 $services = Import-Csv $dirFolder'test.csv'
 [bool]$activeConnection = "True"
+$timer = 300
+
+$smtpServer = 172.16.5.103
+$mailAdmin = patmagpantay@pm.local
+$mailAlert = mission-control@pm.local
+$mailBody = ""
+$mailSubject = ""
+
 
