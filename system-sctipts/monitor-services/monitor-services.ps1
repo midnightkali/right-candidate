f695af2942b0f0afd53cdf5669eec807cd2d5e10
MidnightKali <86851068+midnightkali@users.noreply.github.com>
2022-08-08 02:02:52 -0400

    Update monitor-services.ps1

--- system-sctipts/monitor-services/monitor-services.ps1
+++ system-sctipts/monitor-services/monitor-services.ps1
@@ -1,12 +1,14 @@
 $dirFolder = "C:\promedica\"
 $services = Import-Csv $dirFolder'test.csv'
-[bool]$activeConnection = "True"
+[bool]$activeConnection = 1
 $timer = 300
 
 $smtpServer = 172.16.5.103
-$mailAdmin = patmagpantay@pm.local
-$mailAlert = mission-control@pm.local
+$mailAdmin = "patmagpantay@pm.local"
+$mailAlert = "mission-control@pm.local"
 $mailBody = ""
 $mailSubject = ""
 
-
+while($activeConnection -eq 1){
+    Write-Host "WORKING"
+} 
