999275e933500ca9de61d0ec95f5211a7996641e
MidnightKali <86851068+midnightkali@users.noreply.github.com>
2022-08-08 02:29:10 -0400

    Update monitor-services.ps1

--- system-sctipts/monitor-services/monitor-services.ps1
+++ system-sctipts/monitor-services/monitor-services.ps1
@@ -1,7 +1,7 @@
 $dirFolder = "C:\promedica\"
 $services = Import-Csv $dirFolder'test.csv'
 [bool]$activeConnection = 1
-$timer = 300
+
 
 $smtpServer = 172.16.5.103
 $mailAdmin = "patmagpantay@pm.local"
@@ -10,5 +10,12 @@ $mailBody = ""
 $mailSubject = ""
 
 while($activeConnection -eq 1){
-    Write-Host "WORKING"
+    foreach($service in $services){
+        $checkService = Get-Service | WHERE {$_.Name -eq $service.ServiceName}
+        
+        $mailBody = "Service named: " + $checkService.DisplayName + "is down for hostname " + $env:computername 
+        $mailSubject = $env:computername + " Needs your immediate attention"
+
+        
+    }
 } 
